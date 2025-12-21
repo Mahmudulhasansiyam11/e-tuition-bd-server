bb1b44627881872a4709925a725a6ee9a3f519ab
Mahmudulhasansiyam11 <mahmudulsiyam766@gmail.com>
2025-12-21 19:06:29 +0600

    add transaction history api

--- index.js
+++ index.js
@@ -263,6 +263,12 @@ async function run() {
       res.send(result);
     })
 
+    // get user payment detail
+    app.get("/transaction-history", async (req, res) => {
+      const result = await ordersCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
